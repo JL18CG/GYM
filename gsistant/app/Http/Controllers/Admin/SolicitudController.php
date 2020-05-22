d092427c99ec81b9d0209631d2e6b7629b67fa61
JL18CG <39381181+JL18CG@users.noreply.github.com>
2020-05-22 17:12:50 -0600

    Vistas Admin Protegidas
    Creacion de middleware para proteger las rutas del adinistrador

--- gsistant/app/Http/Controllers/Admin/SolicitudController.php
+++ gsistant/app/Http/Controllers/Admin/SolicitudController.php
@@ -15,6 +15,11 @@ class SolicitudController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+        $this->middleware('auth');
+        $this->middleware('rol.admin');
+    }
+    
     public function index()
     {
 
